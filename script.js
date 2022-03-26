e806ca511290b450e4460bdc79878f92cd950394
AlindaB <alindabyamukama@gmail.com>
2022-03-26 13:27:32 +0300

    code-review: fixed repetitive code blocks with DRY code

--- script.js
+++ script.js
@@ -5,6 +5,15 @@ const nav2 = document.getElementById('nav-2')
 const nav3 = document.getElementById('nav-3')
 const nav4 = document.getElementById('nav-4')
 const nav5 = document.getElementById('nav-5')
+const navItems = [nav1, nav2, nav3, nav4, nav5]
+
+// control navigation animation
+function navAnimation(direction1, direction2) {
+    navItems.forEach((nav, i) =>{
+        //  console.log(`slide-${direction1}-${i + 1}`, `slide-${direction2}-${i + 1}`)
+        nav.classList.replace(`slide-${direction1}-${i + 1}`, `slide-${direction2}-${i + 1}`)
+    })
+}
 
 // Toggle our Navigation Menu Bars
 function toggleNav() {
@@ -14,41 +23,19 @@ function toggleNav() {
     overlay.classList.toggle('overlay-active')
     if(overlay.classList.contains('overlay-active')) {
         // animate -in- overlay
-        overlay.classList.remove('overlay-slide-left')
-        overlay.classList.add('overlay-slide-right')
+        overlay.classList.replace('overlay-slide-left','overlay-slide-right')
         // animate-in nav items
-        nav1.classList.remove('slide-out-1')
-        nav1.classList.add('slide-in-1')
-        nav2.classList.remove('slide-out-2')
-        nav2.classList.add('slide-in-2')
-        nav3.classList.remove('slide-out-3')
-        nav3.classList.add('slide-in-3')
-        nav4.classList.remove('slide-out-4')
-        nav4.classList.add('slide-in-4')
-        nav5.classList.remove('slide-out-5')
-        nav5.classList.add('slide-in-5')
+        navAnimation('out', 'in')
     } else {
         // animate -out- overlay
-        overlay.classList.remove('overlay-slide-right')
-        overlay.classList.add('overlay-slide-left')
+        overlay.classList.replace('overlay-slide-right', 'overlay-slide-left')
         // animate-out nav items
-        nav1.classList.remove('slide-in-1')
-        nav1.classList.add('slide-out-1')
-        nav2.classList.remove('slide-in-2')
-        nav2.classList.add('slide-out-2')
-        nav3.classList.remove('slide-in-3')
-        nav3.classList.add('slide-out-3')
-        nav4.classList.remove('slide-in-4')
-        nav4.classList.add('slide-out-4')
-        nav5.classList.remove('slide-in-5')
-        nav5.classList.add('slide-out-5')
+        navAnimation('in', 'out')
     }
 }
 
 // Event Listeners  
 menuBars.addEventListener('click', toggleNav)
-nav1.addEventListener('click', toggleNav)
-nav2.addEventListener('click', toggleNav)
-nav3.addEventListener('click', toggleNav)
-nav4.addEventListener('click', toggleNav)
-nav5.addEventListener('click', toggleNav)
+navItems.forEach((nav)=>{
+    nav.addEventListener('click', toggleNav)
+})
