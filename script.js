e38faebaa1001eaaaa712159ea72d311d14109c1
AlindaB <alindabyamukama@gmail.com>
2022-03-26 12:50:42 +0300

    created animation with css keyframes and applied animation with javaScript DOM manipulation

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const menuBars = document.getElementById('menu-bars')
 const overlay = document.getElementById('overlay')
 const nav1 = document.getElementById('nav-1')
-const nav2 = document.getElementById('nav-3')
-const nav3 = document.getElementById('nav-4')
-const nav4 = document.getElementById('nav-5')
+const nav2 = document.getElementById('nav-2')
+const nav3 = document.getElementById('nav-3')
+const nav4 = document.getElementById('nav-4')
 const nav5 = document.getElementById('nav-5')
 
 // Toggle our Navigation Menu Bars
@@ -16,10 +16,32 @@ function toggleNav() {
         // animate -in- overlay
         overlay.classList.remove('overlay-slide-left')
         overlay.classList.add('overlay-slide-right')
+        // animate-in nav items
+        nav1.classList.remove('slide-out-1')
+        nav1.classList.add('slide-in-1')
+        nav2.classList.remove('slide-out-2')
+        nav2.classList.add('slide-in-2')
+        nav3.classList.remove('slide-out-3')
+        nav3.classList.add('slide-in-3')
+        nav4.classList.remove('slide-out-4')
+        nav4.classList.add('slide-in-4')
+        nav5.classList.remove('slide-out-5')
+        nav5.classList.add('slide-in-5')
     } else {
         // animate -out- overlay
         overlay.classList.remove('overlay-slide-right')
         overlay.classList.add('overlay-slide-left')
+        // animate-out nav items
+        nav1.classList.remove('slide-in-1')
+        nav1.classList.add('slide-out-1')
+        nav2.classList.remove('slide-in-2')
+        nav2.classList.add('slide-out-2')
+        nav3.classList.remove('slide-in-3')
+        nav3.classList.add('slide-out-3')
+        nav4.classList.remove('slide-in-4')
+        nav4.classList.add('slide-out-4')
+        nav5.classList.remove('slide-in-5')
+        nav5.classList.add('slide-out-5')
     }
 }
 
